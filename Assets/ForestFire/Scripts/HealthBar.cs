39bff063b6c67e5af612b9fd0f39b8df422d1a6a
JAMES MEAD <955568@Swansea.ac.uk>
2022-11-09 17:13:30 +0000

    Fixed Score System

--- Assets/ForestFire/Scripts/HealthBar.cs
+++ Assets/ForestFire/Scripts/HealthBar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,18 +12,23 @@ public class HealthBar : MonoBehaviour
     public GameObject gameOverText; // This is a Text screen displaying "Game Over" text
     public Slider healthSlider;
     public float playerScore;
-
+   
+    public TMP_Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
         playerCurrentHealth = playerMaxHealth; // This command makes the "PC" Max Health equal the Current Health equal the same from the application has started
         gameOverText.SetActive(false); // This sets the text to be invisible
         playerScore = 0;
+
+        scoreText.text = "Fires Extinguished " + playerScore.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+        scoreText.text = "Fires Extinguished " + playerScore.ToString();
+
         healthSlider.value = playerCurrentHealth;
         if (playerCurrentHealth <= 0) // This command checks to see if the "PC" Current Health is equal or lower then zero, if so runs the lines below
         {
